21ff9fe273f8aab562ca56c3651a43408c4a7935
LDAP\tdbao1 <tdbao1@cmcglobal.vn>
2023-02-03 14:37:53 +0700

    Error message in login undefined

--- src/hooks/useUpload.ts
+++ src/hooks/useUpload.ts
@@ -39,7 +39,7 @@ export const useUpload = (t: TFunction<"translation", undefined, "translation">)
     fmData.append("file", file);
     try {
       const {data} = await axios.post(
-        "http://localhost:7000/upload-img",
+        process.env.SERVER_UPLOAD || 'http://54.255.180.132:80/upload-img',
         fmData,
         config
       );
